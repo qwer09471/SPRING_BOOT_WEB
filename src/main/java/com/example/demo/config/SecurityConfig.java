5a71f3038d1305384b8d8ae363d4c131405ec2c3
Your Name <you@example.com>
2024-12-04 11:14:57 +0900

    11주차실습

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,10 +2,9 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,16 +14,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig { // 스프링에서 보안 관리 클래스
 
     @Bean // 명시적 의존성 주입 : Autowired와 다름
-    // 5.7버전 이저 WebSecurityConfigurerAdapter 사용
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless 세션 관리 설정
+            );
 
+        return http.build();
     }
-                
                 
     @Bean // 암호화 설정
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // 비밀번호 암호화 저장
     }
-    }
+}
